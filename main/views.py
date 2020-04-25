671ee01b214e18a23df1eb7db1c35bec020a4cda
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-04-25 09:48:01 +0300

    adding Root function

--- main/views.py
+++ main/views.py
@@ -2,3 +2,14 @@ from flask import render_template,request,redirect,url_for
 from . import main
 from flask import render_template,request,redirect,url_for
 from ..request import get_source,article_source,get_category,get_headlines
+
+
+#our views
+@main.route('/')
+def index():
+    '''
+    Root function returning index/home page with data
+    '''
+    source= get_source()
+    headlines = get_headlines()
+    return render_template('index.html',sources=source, headlines = headlines)
