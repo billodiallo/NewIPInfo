eb44a5d20b42eb360a37d7b5bb45af4ad6cd1ef4
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-04-25 09:53:33 +0300

    adding function that return the various article details page and its data

--- main/views.py
+++ main/views.py
@@ -13,3 +13,13 @@ def index():
     source= get_source()
     headlines = get_headlines()
     return render_template('index.html',sources=source, headlines = headlines)
+
+@main.route('/article/<id>')
+def article(id):
+
+    '''
+    View article page function that returns the various article details page and its data
+    '''
+    # title= 'Articles'
+    articles = article_source(id)
+    return render_template('article.html',articles= articles,id=id )
