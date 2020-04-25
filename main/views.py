a32bb8e4f67be9b67781381f4169a119b2578dad
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-04-25 09:46:42 +0300

    import render_template,get_source,article,categorie,getheadlines

---
+++ main/views.py
@@ -0,0 +1,4 @@
+from flask import render_template,request,redirect,url_for
+from . import main
+from flask import render_template,request,redirect,url_for
+from ..request import get_source,article_source,get_category,get_headlines
